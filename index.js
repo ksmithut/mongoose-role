2816ff0190c21f7e65416d012b9ffb8dec857789
Keith Smith <ksmith.ut@gmail.com>
2014-09-18 18:37:24 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var defaults = require('lodash.defaults');
+
+module.exports = function role(schema, options) {
+
+  // Set the default options
+  options = options || {};
+  defaults(options, {
+    roles: [],
+    accessLevels: {},
+    rolePath: 'role',
+    rolesStaticPath: 'roles',
+    hasAccessMethod: 'hasAccess',
+  });
+
+  // Set the role path
+  schema
+    .path(options.rolePath, String)
+    .path(options.rolePath)
+      .enum(options.roles)
+      .required(true);
+
+  // Expose the roles
+  schema.static(options.rolesStaticPath, options.roles);
+
+  // Set the hasAccess method
+  schema.method(options.hasAccessMethod, function (accessLevel) {
+    // if nothing is passed, then return true
+    if (typeof accessLevel === 'undefined') { return true; }
+
+    var validRoles = options.accessLevels[accessLevel];
+
+    // if there is nothing in the access levels for the given accessLevel, then
+    // the return false.
+    if (!validRoles) { return false; }
+
+    var userRole   = this.get(options.rolePath);
+    return validRoles.indexOf(userRole) !== -1;
+  });
+
+};
---
+++ test/test.js
@@ -0,0 +1,72 @@
+'use strict';
+/* global describe, before, beforeEach, after, afterEach, it */
+/* jshint maxlen: false */
+
+var expect   = require('expect.js');
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+var role     = require('../index');
+var testSchema = {
+  name: String
+};
+
+describe('mongoose-role', function () {
+  // Connect to the database
+  before(function (done) {
+    mongoose.connect('mongodb://127.0.0.1/mongoose-role-test', done);
+  });
+
+  // Delete the database after testing
+  after(function (done) {
+    mongoose.connection.db.dropDatabase(done);
+  });
+
+  // Level 1 tests
+  describe('Level 1', function () {
+
+    it('should give accurate user account lockage', function () {
+      var TestSchema = new Schema(testSchema);
+      TestSchema.plugin(role, {
+        roles: ['user', 'admin'],
+        accessLevels: {
+          'user': ['user', 'admin'],
+          'admin': ['admin']
+        }
+      });
+      var Test = mongoose.model('Test1', TestSchema);
+      var model1 = new Test({name: 'test1', role: 'user'});
+      var model2 = new Test({name: 'test2', role: 'admin'});
+      expect(model1.hasAccess('user')).to.be(true);
+      expect(model1.hasAccess('admin')).to.be(false);
+      expect(model2.hasAccess('user')).to.be(true);
+      expect(model2.hasAccess('admin')).to.be(true);
+      expect(model1.hasAccess()).to.be(true);
+      expect(model1.hasAccess('public')).to.be(false);
+    });
+
+  });
+
+  describe('Level 2', function () {
+
+    it('should not break completely if options aren\'t passed', function (done) {
+      var TestSchema = new Schema(testSchema);
+      TestSchema.plugin(role);
+      var Test = mongoose.model('Test2', TestSchema);
+      var model1 = new Test({name: 'test1', role: 'user'});
+      var model2 = new Test({name: 'test2', role: 'admin'});
+      expect(model1.hasAccess('user')).to.be(false);
+      expect(model1.hasAccess('admin')).to.be(false);
+      expect(model2.hasAccess('user')).to.be(false);
+      expect(model2.hasAccess('admin')).to.be(false);
+      expect(model1.hasAccess()).to.be(true);
+      expect(model1.hasAccess('public')).to.be(false);
+      model1.save(function (err, model) {
+        expect(err).to.be.ok();
+        expect(err.errors.role.type).to.be('enum');
+        done();
+      });
+    });
+
+  });
+
+});
